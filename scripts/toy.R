39b91921cb25cdb380f4c2812f952050a62e9004
Hugo.fxy <hugo.fxy@gmail.com>
2015-04-30 13:52:22 +0200

    reorganized, but nnet is not working.

--- scripts/toy.R
+++ scripts/toy.R
@@ -4,6 +4,10 @@ inputData <- read.csv('../data/train.csv')
 Num.totalSize <- dim(inputData)[1]
 Num.totalCol <- dim(inputData)[2]
 library(nnet)
+library(e1071)
+library('tree')
+library('lazy')
+
 set.seed(0)
 
 Num.ts.size <- round(Num.totalSize/10)
@@ -19,39 +23,62 @@ Y.ts <- inputData[i.ts, Num.totalCol]
 Y.tr <- inputData[i.tr, Num.totalCol]
 Num.labels <- length(levels(Y.ts))
 Y.tsi <- as.numeric(sub('Class_', '', Y.ts))
-#Y.tri <- as.numeric(sub('Class_', '', Y.tr))
-
-Y.tsv <- class.ind(Y.ts)
-Y.trv <- class.ind(Y.tr)
-
+Y.tri <- as.numeric(sub('Class_', '', Y.tr))
 
 # Shuffling training data
 index <- sample(1:Num.tr.size)
 X.tr <- X.tr[index, ]
-Y.trv <- Y.trv[index, ]
+Y.tr <- Y.tr[index]
+Y.tri <- Y.tri[index]
+
+using.tree <- function(trainX, trainY){
+    trainXY.combined <- cbind(trainX, trainY)
+    model <- tree(trainY~., trainXY.combined)
+}
+
+using.nnet <- function(trainX, trainY){
+    hiddenLayer_size <- 8
+    iterations <- 200
+    print(paste("Hidden Layer size: ", hiddenLayer_size))
+    print(paste("Max iterations: ", iterations))
+    trainY_matrix = class.ind(trainY)
+    model <- nnet(trainX, trainY_matrix, , size = hiddenLayer_size,  mmaxit = iterations, rang = 0.1, decay = 5e-4, MaxNWts=1000000, linout = FALSE, trace=FALSE)
+}
 
 BERate_list = NULL
 mis_error_list = NULL
 logloss_list = NULL
-for(hiddenLayer_size in 1:15)
-{
+# for(hiddenLayer_size in 1:15)
+# {
 # Building a Neural Network
-#hiddenLayer_size <- 7
-iterations <- 100
-print(paste("Hidden Layer size: ", hiddenLayer_size))
-print(paste("Max iterations: ", iterations))
-model <- nnet(X.tr, Y.trv, size = hiddenLayer_size,  rang = 0.1, decay = 5e-4, mmaxit = iterations, trace=FALSE, MaxNWts=1000000, linout = FALSE)
+
+
+model <- using.nnet(X.tr, Y.tr)
 result <- predict(model, X.ts)
 result.label <- max.col(result)
 
-# accuracy of classification
-accuracy <- mean(Y.tsi==result.label)
+# #Buiding SVM # TODO: still have problems
+# model<-svm(Y.tri,X.tr)
+# result.label <- predict(model, X.ts)
+
+# # Building Tree
+# model <- using.tree(X.tr, Y.tr)
+# result<-predict(model, X.ts)
+# result.label <- max.col(result)
+
+# # Building KNN model with 'lazy' package # has problem
+# model <- lazy(Y.tr~., trainXY.combined)
+# result<- predict(model, X.ts)
+# result.label <- max.col(result)
+
+# # accuracy of classification
+#accuracy <- mean(Y.tsi==result.label)
 #print(paste("Classification accuracy: ", accuracy))
 
 # mis-classification error
-mis_error <- 1-accuracy
+mis_error <- mean(Y.tsi!=result.label)
 mis_error_list <- c(mis_error_list, mis_error)
-print(paste("Mis-Classification error rate: ", accuracy))
+print(paste("Mis-Classification error rate: ", mis_error))
 
 # balanced error rate
 ## The index of column means the predicted results,
@@ -89,10 +116,10 @@ logloss <- -logloss/Num.ts.size
 logloss_list <- c(logloss_list, logloss)
 print(paste("logloss: ", logloss))
 
-} #line 34
-x <- 1:15
-par(mfrow=c(1,1))
-#plot(c(1, 15), c(0, 1), type="n")
-plot(x, BERate_list, xlim=c(1, 15), ylim=c(0, 1), type="l", col="red")
-lines(x, mis_error_list, col="blue")
-lines(x, logloss_list, col="black")
+# } #line 34
+# x <- 1:15
+# par(mfrow=c(1,1))
+# #plot(c(1, 15), c(0, 1), type="n")
+# plot(x, BERate_list, xlim=c(1, 15), ylim=c(0, 1), type="l", col="red")
+# lines(x, mis_error_list, col="blue")
+# lines(x, logloss_list, col="black")
